0d07b91bcfbd88264048cb81e7ac1d19faca5b23
kitearc <yeonholee.dev@gmail.com>
2022-12-10 14:41:26 +0900

    first commit

---
+++ Baekjoon/[16394] 홍익대학교/Main16394.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class Main16394 {
+
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		
+		System.out.println(A-1946);
+ 
+		sc.close();
+	}
+}
